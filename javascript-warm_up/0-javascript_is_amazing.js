b885772e47f705916ea33ac88d7b541b6c4faaf1
judyloveth <judyohwo29@gmail.com>
2023-09-09 03:23:15 +0100

    new repo initialised

---
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// a constant variable
+const myVar = 'JavaScript is amazing';
+
+console.log(myVar);
---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+let statement = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; i < statement.length; i++) {
+  console.log(statement[i]);
+}
