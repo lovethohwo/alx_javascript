a7e944901054fb1569e60b0c4994d465c5141ed1
judyloveth <judyohwo29@gmail.com>
2023-09-09 04:12:57 +0100

    updates

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+// create a class with its constructor
+class Rectangle {
+    constructor(w, h) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  
+  // create a module for exports
+  module.exports = Rectangle;
---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// create a class with its constructor
+class Rectangle {
+    constructor(w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  }
+  
+  // create a module for exports
+  module.exports = Rectangle;
